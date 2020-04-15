5834984abbd5809428ccc67a9fc816711529373a
prashantgmicro <pgole31@gmail.com>
2020-04-15 19:34:16 +0530

    php convert3

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THHLWSN"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-<?php include "inqfrm.php";?>
+
 <div class="head-section">
     <header class="header-section" data-offset-top="1" data-spy="affix" id="tasngsite"></header>
  
@@ -410,6 +410,7 @@ Certificate of Merit</strong> in recognition of excellent performance in the fil
   </script> 
   <script src='assets/js/slick.min.js' type='text/javascript'>
   </script>
+  <?php include "inqfrm.php";?>
 </body>
 <!-- pnn2 -->
 </html>
